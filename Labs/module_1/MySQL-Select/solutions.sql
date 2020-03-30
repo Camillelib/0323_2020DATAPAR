f8292a251f8bfe91a330a1f6c689ce061902ffce
Camille Libert <camille.libert.coubard@gmail.com>
2020-03-30 15:39:18 +0200

    week2

--- Labs/module_1/MySQL-Select/solutions.sql
+++ Labs/module_1/MySQL-Select/solutions.sql
@@ -1,42 +1,41 @@
 SELECT a.au_id,a.au_lname,a.au_fname, tt.title, p.pub_name
 FROM authors a
-left join titleauthor t
+inner join titleauthor t
 on a.au_id = t.au_id 
-left join titles tt
+inner join titles tt
 on t.title_id = tt.title_id
-left join publishers p
+inner join publishers p
 on p.pub_id = tt.pub_id;
 
 SELECT a.au_id,a.au_lname,a.au_fname, p.pub_name, count(tt.title) as 'TITLE COUNT'
 FROM authors a
-left join titleauthor t
+inner join titleauthor t
 on a.au_id = t.au_id 
-left join titles tt
+inner join titles tt
 on t.title_id = tt.title_id
-left join publishers p
+inner join publishers p
 on p.pub_id = tt.pub_id
 group by tt.title;
 
-SELECT a.au_id,a.au_lname,a.au_fname, s.qty as TOTAL
+SELECT a.au_id, au_lname, au_fname, sum(s.qty)
 FROM authors a
-left join titleauthor t
+inner join titleauthor t
 on a.au_id = t.au_id 
-left join titles tt
+inner join titles tt
 on t.title_id = tt.title_id
-left join sales s
+inner join sales s
 on s.title_id = tt.title_id
-order by s.qty desc
+group by a.au_id
+order by sum(s.qty) desc
 limit 3;
 
-SELECT a.au_id,a.au_lname,a.au_fname, IFNULL(s.qty,0) as TOTAL
+SELECT a.au_id, au_lname, au_fname, IFNULL(sum(s.qty),0) as TOTAL
 FROM authors a
-left join titleauthor t
+inner join titleauthor t
 on a.au_id = t.au_id 
-left join titles tt
+inner join titles tt
 on t.title_id = tt.title_id
-left join sales s
+inner join sales s
 on s.title_id = tt.title_id
-order by s.qty desc;
-
-
-
+group by a.au_id
+order by sum(s.qty) desc
