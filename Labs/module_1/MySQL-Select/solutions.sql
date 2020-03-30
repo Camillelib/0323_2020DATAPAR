587cdabefc3ca2fdcd4ff1842128815c880f78af
Camille Libert <camille.libert.coubard@gmail.com>
2020-03-30 16:13:44 +0200

    week2

--- Labs/module_1/MySQL-Select/solutions.sql
+++ Labs/module_1/MySQL-Select/solutions.sql
@@ -7,15 +7,15 @@ on t.title_id = tt.title_id
 inner join publishers p
 on p.pub_id = tt.pub_id;
 
-SELECT a.au_id,a.au_lname,a.au_fname, p.pub_name, count(tt.title) as 'TITLE COUNT'
+SELECT a.au_id,a.au_lname,a.au_fname, count(tt.title) as 'TITLE COUNT', p.pub_name
 FROM authors a
-inner join titleauthor t
+join titleauthor t
 on a.au_id = t.au_id 
-inner join titles tt
+join titles tt
 on t.title_id = tt.title_id
-inner join publishers p
+join publishers p
 on p.pub_id = tt.pub_id
-group by tt.title;
+group by a.au_id, p.pub_name
 
 SELECT a.au_id, au_lname, au_fname, sum(s.qty)
 FROM authors a
@@ -25,7 +25,7 @@ inner join titles tt
 on t.title_id = tt.title_id
 inner join sales s
 on s.title_id = tt.title_id
-group by a.au_id
+group by a.au_id	
 order by sum(s.qty) desc
 limit 3;
 
