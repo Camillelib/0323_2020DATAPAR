32c2d9d80e2ae8758f7c9f29a334633350edac95
Camille Libert <camille.libert.coubard@gmail.com>
2020-04-02 12:09:22 +0200

    Updating SQL Finance

--- Labs/module_1/MySQL_LAB_Finance_Data/solutions.sql
+++ Labs/module_1/MySQL_LAB_Finance_Data/solutions.sql
@@ -12,12 +12,33 @@ limit 5;
 select *
 from finance.order;
 
+select account_id, group_concat(distinct bank_to), group_concat(distinct amount), count(distinct amount) as diff
+from finance.order
+where k_symbol ='SIPO'
+group by account_id
+having diff>1;
+
 #Challenge 3 - Best Selling Districts
 select account.account_id, amount, account.district_id
 from loan
 inner join account
 on account.account_id = loan.account_id
 order by amount desc;
+
+#Solution in class
+select district_id, max(amount) as amount, group_concat(account_id) 
+from xxx
+group by district_id
+order by 2 desc;
+
+#Solution in class ok
+select x1.district_id, x1.amount, xxx.account_id
+from(select district_id, max(amount) as amount, group_concat(account_id)
+from xxx
+group by district_id) as x1
+inner join xxx on xxx.district_id = x1.district_id
+where xxx.amount=x1.amount
+order by 2 desc;
 
 #Challenge 4 - Best Selling Districts UPDATED
 select account.district_id, sum(amount) as total_amount
@@ -27,6 +48,11 @@ on account.account_id = loan.account_id
 group by district_id
 order by total_amount desc;
 
+select district_id, sum(amount) as total_amount, group_concat(account_id) 
+from xxx
+group by district_id
+order by 2 desc;
+
 #Challenge 5 - Best Selling Districts UPDATED
 select account.district_id, sum(amount), round(AVG(amount))
 from loan
@@ -34,5 +60,43 @@ inner join account
 on account.account_id = loan.account_id
 group by district_id;
 
-#Bonus Challenge - Salary/Loan balance
-#Detect the Average Salary/ Average Loan/ Average Insurance balance in every district.
+#Calculating the median:
+#merge every single row with every single row:
+select *
+from xxx as x1, xxx as x2;
+
+select count(*) as one_table 
+from xxx;
+
+select count(*)
+from xxx as x1, xxx as x2;
+
+# Calculating number of rows ranked
+select * 
+from (select x1.district_id, x1.amount, count(x2.amount) ranking
+from xxx as x1, xxx as x2
+where x1.amount<=x2.amount and x1.district_id=x2.district_id
+group by x1.district_id, x1.amount
+order by x1.district_id, x1.amount, x2.amount) ranked;
+
+#calculated the middle number of rows for each district
+select x1.district_id, ceil(count(x1.amount)/2) ranking
+from xxx x1
+group by district_id
+order by district_id;
+
+#computing the median for each district: final output
+select district_id, amount, ranking
+from (select x1.district_id, x1.amount, count(x2.amount) ranking
+from xxx as x1, xxx as x2
+where x1.amount<x2.amount and x1.district_id=x2.district_id
+group by x1.district_id, x1.amount
+order by x1.district_id, x1.amount, x2.amount) ranked
+where exists (
+select x1.district_id, ceil(count(x1.amount)/2) ranking
+from xxx x1
+group by district_id
+having ranked.district_id=x1.district_id 
+and ranked.ranking=ranking)
+order by amount
+;
